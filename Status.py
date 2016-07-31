39b2d6b78d74524f2a893da7a0466b2d28f5dfc4
PaleBoy <palesamurai@hotmail.com>
2016-07-31 16:00:51 -0400

    added a bunch of stuff

--- Status.py
+++ Status.py
@@ -3,11 +3,16 @@ from i3pystatus import Status
 
 status = Status()
 
-status.register("clock",
-    format="%a %-d %b %X",)
+status.register("clock", format="%a %-d %b %I : %M",)
 
-status.register("temp",
-    format="{temp:.0f}°C",)
+status.register("temp", format="{temp:.0f}°C",)
 
+status.register("cpu_usage", format=" CPU: {usage:02}%",)
+
+status.register("mem", format="{avail_mem} mb",)
+
+status.register("network", interface="wlan0", format_up="{essid} {quality:03.0f}%",)
+
+status.register("bitcoin", format="{symbol} {status}{last_price}",)
 
 status.run()
