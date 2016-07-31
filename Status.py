3269c2f21c7c0f5ace8c0112fe212dadf6e52a3c
PaleBoy <palesamurai@hotmail.com>
2016-07-31 13:57:59 -0400

    Added clock and temp.

--- Status.py
+++ Status.py
@@ -2,6 +2,9 @@
 from i3pystatus import Status
 
 status = Status()
- blah testing
 
- blah more testing
+status.register("clock",
+    format="%a %-d %b %X",)
+
+status.register("temp",
+    format="{temp:.0f}°C",)
